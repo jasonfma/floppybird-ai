224a4d564ecb587bd3db95425927ae07ca1b503d
Jason Ma <ma.jasonf@gmail.com>
2017-04-20 01:15:27 -0700

    Fork of https://github.com/spritekitbook/flappybird-swift Lesson 9 - Adds AIPlayer

---
+++ FloppyBird/AIPlayer.swift
@@ -0,0 +1,23 @@
+//
+//  AIPlayer.swift
+//  FloppyBird
+//
+//  Created by Jason Ma on 4/20/17.
+//  Copyright © 2017 SpriteKit Book. All rights reserved.
+//
+
+import Foundation
+
+class AIPlayer {
+    class func play(logController: LogController, floppy: Floppy) {
+        for node in logController.children {
+            if let log = node as? Log {
+                NSLog("\(log.position)");
+            }
+        }
+        
+        if (floppy.position.y < 200) {
+            floppy.fly();
+        }
+    }
+}
